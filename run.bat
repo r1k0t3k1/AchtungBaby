54182f8a8f163404bf56492a0123e3ac141a0720
r1k0t3k1 <57973603+r1k0t3k1@users.noreply.github.com>
2025-04-29 11:21:18 +0900

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,15 +1,9 @@
 set COR_ENABLE_PROFILING=1
 set COR_PROFILER={5c8e9579-53b9-5a69-6a75-2d232518df35}
-REM set COR_PROFILER_DLL=%~dp0\target\debug\achtung_baby.dll
-
-REG ADD "HKCU\Software\Classes\CLSID\{5c8e9579-53b9-5a69-6a75-2d232518df35}" /f
-REG ADD "HKCU\Software\Classes\CLSID\{5c8e9579-53b9-5a69-6a75-2d232518df35}\InprocServer32" /f
-REM REG ADD "HKCU\Software\Classes\CLSID\{5c8e9579-53b9-5a69-6a75-2d232518df35}\InprocServer32" /ve /t REG_SZ /d "%~dp0\target\debug\achtung_baby.dll" /f
-REG ADD "HKCU\Software\Classes\CLSID\{5c8e9579-53b9-5a69-6a75-2d232518df35}\InprocServer32" /ve /t REG_SZ /d "%~dp0\target\release\achtung_baby.dll" /f
+set COR_PROFILER_DLL=%~dp0\target\debug\achtung_baby.dll
 
 powershell
 
 set COR_ENABLE_PROFILING=
 set COR_PROFILER=
-
-REG DELETE "HKCU\Software\Classes\CLSID\{5c8e9579-53b9-5a69-6a75-2d232518df35}" /f
+set COR_PROFILER_DLL=
